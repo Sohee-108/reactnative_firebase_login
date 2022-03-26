f3c23aa2cbbfc9f38f06ec0922bda7a223ce46ad
ddohing <dnflwlq004@naver.com>
2022-03-26 17:41:25 +0900

    first

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const AppStack = createStackNavigator();
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen
+          name="Onbording"
+          component={OnboardingScreen}
+          options={{headerShown: false}}
+        />
+        <AppStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
