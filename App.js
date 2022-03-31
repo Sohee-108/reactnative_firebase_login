dff5026adb6feda02d1cf5a8c2749bfb0179f5bf
ddohing <dnflwlq004@naver.com>
2022-03-31 23:54:58 +0900

    Firebase Storage Tutorial for Storing Photos

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import {LogBox} from 'react-native';
 import Provider from './navigation';
+
+LogBox.ignoreLogs([
+  'componentWillReceiveProps has been renamed, and is not recommended for use.',
+  '`useNativeDriver` was not specified.',
+  'RCTBridge required dispatch_sync to load RNGestureHandlerModule.',
+  'Found screens with the same name nested inside one another.',
+]);
 
 const App = () => {
   return <Provider></Provider>;
--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -1,31 +1,88 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Alert} from 'react-native';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {ImagePicker} from 'react-native-image-crop-picker';
+import storage from '@react-native-firebase/storage';
 
-import {InputField, InputWrapper} from '../styles/AddPost';
+import {
+  InputField,
+  InputWrapper,
+  AddImage,
+  SubmitBtn,
+  SubmitBtnText,
+} from '../styles/AddPost';
 
 const AddPostScreen = () => {
+  const [image, setImage] = useState(null);
+  const [uploading, setuploading] = useState(false);
+  const [transferred, settransferred] = useState(0);
+
+  const takePhotoFromCamera = () => {
+    ImagePicker.openCamera({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path;
+      setImage(imageUrl);
+    });
+  };
+
+  const choosePhotoFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path;
+      setImage(imageUrl);
+    });
+  };
+
+  const submitPost = async () => {
+    const uploadUri = image;
+    let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
+    setuploading(true);
+    try {
+      await storage().ref(filename).putFile(uploadUri);
+      setuploading(false);
+      Alert.alert(
+        'Image uploaded!',
+        'Your image has been uploaded to the Firebase Cloud Storage Successfully',
+      );
+    } catch (e) {
+      console.log(e);
+    }
+    setImage(null);
+  };
+
   return (
     <View style={styles.container}>
       <InputWrapper>
+        {image != null ? <AddImage source={{uri: image}} /> : null}
         <InputField
           placeholder="What`s on your mind?"
           multiline
           numberOfLined={4}
         />
+        <SubmitBtn onPress={submitPost}>
+          <SubmitBtnText>Post</SubmitBtnText>
+        </SubmitBtn>
       </InputWrapper>
-      <ActionButton buttonColor="rgba(231,76,60,1)">
+      <ActionButton buttonColor="#2e64e5">
         <ActionButton.Item
           buttonColor="#9b59b6"
           title="Take Photo"
-          onPress={() => console.log('notes tapped!')}>
+          onPress={takePhotoFromCamera}>
           <Icon name="camera-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
         <ActionButton.Item
           buttonColor="#3498db"
           title="Choose pgoto"
-          onPress={() => {}}>
+          onPress={choosePhotoFromLibrary}>
           <Icon name="md-images-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
       </ActionButton>
